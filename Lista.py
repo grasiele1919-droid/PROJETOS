b4e9d8ce4cfaaa59d34565e67d6e106a262bb7ca
grasiele1919-droid <grasiele1919@gmail.com>
2026-05-06 20:29:41 -0300

    06/05 - Listas

--- Lista.py
+++ Lista.py
@@ -4,3 +4,4 @@ Print("Lista completa de produtos: "Produtos)
 print("Primeiro produto da lista: "Produtos[0]) 
 print("Último produto da lista: "Produtos[-1])
 print("Quantidade de produtos na lista: "len(Produtos))
+

09e9fa1d39a0571c1822a9b83d7cb8e8175f9843
grasiele1919-droid <grasiele1919@gmail.com>
2026-05-13 21:58:26 -0300

    atividade

--- Idade_while.py
+++ Idade_while.py
@@ -1,4 +1,5 @@
 idade = -1
+tentativas = -1
 
 while idade <0 or idade > 120:
     idade = int(input("Digite a idade válida (0 a 120): "))
@@ -6,4 +7,6 @@ while idade <0 or idade > 120:
     if idade <0 or idade > 120:
         print("Errou! Idade invalida, tente novamente.")
 
-print (f"Obrigada! A idade digitada digitada é {idade}")     
+tentativas = tentativas +1
+
+print (f"Obrigada! A idade digitada digitada é {idade}. Acertou na {tentativas} tentativa.")     

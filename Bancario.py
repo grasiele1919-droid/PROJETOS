64e7b03d021bf121283c69e634a15df61e761841
grasiele1919-doid <grasiele1919@gmail.com>
2026-06-08 20:29:07 -0300

    08-06

--- Bancario.py
+++ Bancario.py
@@ -5,24 +5,39 @@ def menu_deposito():
     2 - Sacar
     3 - Ver saldo
     0 - Sair
-    +++++++++++++++++++++++++
+    +++++++++++++++++++++++++ 
     """
         print(msg)
-def deposito():
-        valor = float(input("Digite quanto deseja depositar: R$ "))
-        saldo_total = saldo + valor
-        print(f"Deposito feito de R$ {valor}.")
-        
-def sacar():
-      
 
+def deposito(saldo):
+        valor = float(input("Digite valor que deseja depositar: R$ "))
+        saldo += valor #(saldo = saldo + valor)
+        print(f"Depósito de R$ {valor} realizado.")
+        return saldo
+
+def sacar(saldo):
+        saque = float(input("Digite o valor que deseja sacar: R$ "))
+        if saque > saldo:
+            print("Saldo insuficiente")
+            return saldo #devolve o saldo atualizado para o escopo global.
+        saldo -= saque
+        print(f" Valor sacado é de R$ {saque}.")
+        return saldo          
+      
+def mostrar_saldo(saldo):
+      print (F"Saldo total de R$ {saldo}")
+      
 saldo = 0.0
 
 while True:
     menu_deposito()
-    deposito = input("Digite uma opção: ")
+    opcao = input("Digite uma opção: ")
         
-    if deposito == "0":
+    if opcao == "0":
         print("Saindo...")
         break
-    elif deposito == "1": deposito()
+    elif opcao == "1": saldo = deposito(saldo) #return no saldo ela vai atualizar do locar para a varial global
+    elif opcao == "2": saldo = sacar (saldo)
+    elif opcao == "3": mostrar_saldo(saldo)
+    else:
+          print("Opção invalida")

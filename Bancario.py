c0f36f03abe84ef9ea3513c2007757ee63396342
Grasiele Batista <grasiele1919@gmail.com>
2026-06-08 16:08:17 -0300

    08/06

--- Bancario.py
+++ Bancario.py
@@ -8,21 +8,37 @@ def menu_deposito():
     +++++++++++++++++++++++++
     """
         print(msg)
-def deposito():
-        valor = float(input("Digite quanto deseja depositar: R$ "))
-        saldo_total = saldo + valor
-        print(f"Deposito feito de R$ {valor}.")
-        
-def sacar():
-      
 
+def deposito(saldo):
+        valor = float(input("Digite quanto deseja depositar: R$ "))
+        saldo += valor
+        print(f"Deposito feito de R$ {valor}.")
+        return saldo
+
+def sacar(saldo):
+        saque = float(input("Digite o valor que desaja sacar: R$ "))
+
+        if saque <= saldo:
+            saldo -= saque
+            print(f" Valor sacado de R$ {saque}")
+            return saldo
+        else:
+            print("Saldo insuficiente")
+      
+def mostrar_saldo(saldo):
+      print (F"Saldo total de R$ {saldo}")
+      
 saldo = 0.0
 
 while True:
     menu_deposito()
-    deposito = input("Digite uma opção: ")
+    opcao = input("Digite uma opção: ")
         
-    if deposito == "0":
+    if opcao == "0":
         print("Saindo...")
         break
-    elif deposito == "1": deposito()
+    elif opcao == "1": saldo = deposito(saldo)
+    elif opcao == "2": saldo = sacar (saldo)
+    elif opcao == "3": mostrar_saldo(saldo)
+    else:
+          print("Opção invalida")

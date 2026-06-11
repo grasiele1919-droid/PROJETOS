26b3a0be30062c8e7601abca39741d154a403f22
grasiele1919-doid <grasiele1919@gmail.com>
2026-06-10 21:23:00 -0300

    10-06

--- Ciclo_3/teste_Classe_objetos.py
+++ Ciclo_3/teste_Classe_objetos.py
@@ -7,7 +7,38 @@ class Produtos:
     def exibir(self):
         print(f"Produto:{self.nome} | Preço:{self.preco} |Quantidade:{self.qnt}")
 
-produto_1 = Produtos(nome="arroz", preco=7.0, qnt=10) #("arroz", 7.0, 10)
-produto_2 = Produtos("feijão", 4.0, 8)
+def exibir_menu():
+    print("____________________")
+    print("1 - Cadastro de produtos")
+    print("2 - Exibir produtos")
+    print("0 - Sair")
+    print("____________________")
 
-produto_2.exibir()
+def cadastrar_produtos():
+    print("\n CADASTRANDO PRODUTO...")
+    nome = input("Digite o nome do produto: ")
+    preco = float(input("Digite o preço do produto: "))
+    qnt = int(input("Digite o quantidade do produto: "))
+    produto = Produtos(nome,preco, qnt)
+    produtos.append(produto)
+
+def exibir_produtos():
+    if not produtos:
+        print("Não há produtos cadastrados")
+        return
+    for produto in produtos:
+        print("---------------------------")
+        produto.exibir()
+
+produtos = []
+while True:
+    exibir_menu()
+    opcao = input("Escolha a opção: ")
+
+    if opcao == "0":
+        break
+    elif opcao =="1":cadastrar_produtos()
+    elif opcao =="2": exibir_produtos()
+    else:
+        print("Opção invalida!")
+
